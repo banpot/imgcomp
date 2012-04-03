bd90ef5c42ee3490e560d93435a73e5be05a8d38
Banpot Dolwithayakul <banpot@localhost.localdomain>
2012-04-03 15:04:21 +0700

    fix 2d array allocations

--- imgcomp.h
+++ imgcomp.h
@@ -27,7 +27,7 @@ int height,width,height2,width2;
 float imgcomp(char* filename1, char* filename2, int CompareValueType){
 
 // Debug
-printf("%s\n%s", filename1, filename2);
+printf("Filename 1: %s\nFilename 2: %s\n", filename1, filename2);
 
 // Fetching filename 1
 
@@ -37,11 +37,17 @@ printf("Data size is (#cols, #rows) = (%d, %d)\n", img.cols, img.rows);
 width = img.cols;
 height = img.rows;
 
-r1 = (double**)malloc(height*width*sizeof(double *));
-g1 = (double**)malloc(height*width*sizeof(double *));
-b1 = (double**)malloc(height*width*sizeof(double *));
+r1 = (double**)malloc(height*sizeof(double *));
+g1 = (double**)malloc(height*sizeof(double *));
+b1 = (double**)malloc(height*sizeof(double *));
 
-/*
+for (int i=0;i<height;i++){
+r1[i] = (double*)malloc(height*sizeof(double));
+g1[i] = (double*)malloc(height*sizeof(double));
+b1[i] = (double*)malloc(height*sizeof(double));
+}
+
+
 for (int y = 0; y < img.rows; ++y) {
 	for (int x = 0; x < img.cols; ++x)
 	{
@@ -51,21 +57,27 @@ for (int y = 0; y < img.rows; ++y) {
 		b1[y][x] = int( vec3b[2]);
 	}
 }
-*/
+
 
 cv::Mat_<cv::Vec3b> img2 = cv::imread(filename2, 1);
 printf("Data size is (#cols, #rows) = (%d, %d)\n", img2.cols, img2.rows);
 width2 = img2.cols;
 height2 = img2.rows;
 
-r2 = (double**)malloc(height*width*sizeof(double *));
-g2 = (double**)malloc(height*width*sizeof(double *));
-b2 = (double**)malloc(height*width*sizeof(double *));
+r2 = (double**)malloc(height*sizeof(double *));
+g2 = (double**)malloc(height*sizeof(double *));
+b2 = (double**)malloc(height*sizeof(double *));
+
+for (int i=0;i<height;i++){
+r2[i] = (double*)malloc(height*sizeof(double));
+g2[i] = (double*)malloc(height*sizeof(double));
+b2[i] = (double*)malloc(height*sizeof(double));
+}
 
 // Image not equal = exit
 if (width!=width2 || height!=height2) {printf("\nTwo images is not in equal size.\n");return(IMGCOMP_PICNOTEQUAL);}
 
-/*
+
 for (int y = 0; y < img2.rows; ++y) {
 	for (int x = 0; x < img2.cols; ++x)
 	{
@@ -76,6 +88,6 @@ for (int y = 0; y < img2.rows; ++y) {
 	}
 }
 
-*/
+
 
 } // End function
